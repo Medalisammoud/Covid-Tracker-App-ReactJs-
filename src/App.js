3fbbcaae25b80478d74aeaef5e100446ec3f7016
Mohamed Ali Sammoud <Mohamed.Ali.Sammoud@gmail.com>
2021-09-16 23:10:41 +0100

    Component CountryPicker

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import styles from "./App.module.css";
 import { Cards, Chart, CountryPicker } from "./components";
 
 import { fetchData } from "./api";
+import image from "./images/image.png";
 
 class App extends React.Component {
   state = {
     data: {},
+    country: "",
   };
 
   async componentDidMount() {
@@ -15,13 +17,19 @@ class App extends React.Component {
 
     this.setState({ data: fetcheddata });
   }
+
+  handleCountryChange = async (country) => {
+    const fetcheddata = await fetchData(country);
+    this.setState({ data: fetcheddata, country });
+  };
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
     return (
       <div className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
         <Cards data={data} />
-        <CountryPicker />
-        <Chart />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
       </div>
     );
   }
--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 const url = "https://covid19.mathdro.id/api";
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+  let changeableUrl = url;
+
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`;
+  }
   try {
-    const { data } = await axios.get(url);
+    const { data } = await axios.get(changeableUrl);
 
     const { confirmed, recovered, deaths, lastUpdate } = data;
 
@@ -24,6 +29,18 @@ export const fetchDailyData = async () => {
     }));
 
     return modifiedData;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchCountries = async () => {
+  try {
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
+
+    return countries.map((country) => country.name);
   } catch (error) {
     console.log(error);
   }
